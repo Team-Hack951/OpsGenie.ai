d2db8e68b20a95d298e37e227d2a07dd1611b95e
smquadriAptsi <msquadri@aptsi.com>
2025-05-13 01:27:06 +0530

    code changes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Request
-from app.slack_handler import handle_slack_event
+# main.py or app/main.py
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.post("/slack/events")
-async def slack_events(request: Request):
-    return await handle_slack_event(request)
+@app.get("/")
+def read_root():
+    return {"msg": "Hello from FastAPI on Render"}
